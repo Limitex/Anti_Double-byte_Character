bcf12759f5c7e50676db0707232f37a38853cce0
Limitex <76650151+Limitex@users.noreply.github.com>
2021-10-05 19:05:32 +0900

    add program

--- Anti_Double-byte_Character/Form1.Designer.cs
+++ Anti_Double-byte_Character/Form1.Designer.cs
@@ -34,6 +34,7 @@ namespace Anti_Double_byte_Character
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.choiceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -47,16 +48,24 @@ namespace Anti_Double_byte_Character
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
+            this.exitToolStripMenuItem,
+            this.choiceToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(94, 26);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // choiceToolStripMenuItem
+            // 
+            this.choiceToolStripMenuItem.Name = "choiceToolStripMenuItem";
+            this.choiceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.choiceToolStripMenuItem.Text = "Choice";
+            this.choiceToolStripMenuItem.Click += new System.EventHandler(this.choiceToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -75,6 +84,7 @@ namespace Anti_Double_byte_Character
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem choiceToolStripMenuItem;
     }
 }
 
